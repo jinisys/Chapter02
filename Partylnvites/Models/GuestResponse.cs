73a677d64e611cad0b1dfe9e1a394b2fafb38038
KWONOS <oh-seung.kwon@jinisys.net>
2017-07-11 17:34:21 +0900

    TEST

--- Partylnvites/Models/GuestResponse.cs
+++ Partylnvites/Models/GuestResponse.cs
@@ -14,8 +14,7 @@ namespace Partylnvites.Models
         [Required(ErrorMessage ="Please enter your email address")]
         [RegularExpression(".+\\@.+\\..+",ErrorMessage ="Please enter a valid email address")]
         public string Email { get; set; }
-
-
+        
         [Required(ErrorMessage ="Please enter your phone number")]
         public string Phone { get; set; }
 
